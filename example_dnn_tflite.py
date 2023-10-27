de3a1d437b7106cf2e868ebcd01303772383f648
cksajil <sajilck@gmail.com>
2023-10-27 11:13:08 +0530

    Adding DNN example draft

--- example_dnn_tflite.py
+++ example_dnn_tflite.py
@@ -1,10 +1,20 @@
+import tensorflow as tf
+from os.path import join
 from keras.layers import Dense
 from keras.models import Sequential
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_regression
+from hex_converter import hex_to_c_array
 
 EPOCHS = 500
 indx = range(EPOCHS)
+TFLITE_FOLDER = "tflite"
+TFLITE_FILE_NAME = "dnn_model.tflite"
+
+HEADER_FILES_FOLDER = "headerfiles"
+HEADER_FILE_NAME = "dnn_model.h"
+
+
 
 X, y = make_regression(n_samples=200, n_features=2, n_targets=1)
 
@@ -29,6 +39,19 @@ plt.xlabel("Epochs")
 plt.ylabel("Mean Absolute Error")
 plt.legend(["train", "validation"], loc = "upper right")
 plt.show()
+
+
+converter = tf.lite.TFLiteConverter.from_keras_model(dnn_model)
+converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+tflite_model = converter.convert()
+
+with open(join(TFLITE_FOLDER, TFLITE_FILE_NAME), "wb") as f:
+        f.write(tflite_model)
+
+
+with open(join(HEADER_FILES_FOLDER, HEADER_FILE_NAME), "wb") as f:
+        f.write(hex_to_c_array(tflite_model, HEADER_FILE_NAME))
+
 
 print("Sample I/O Pairs")
 print(X[4,:])
