90741d593200f02981087a0153e55d184423518a
cksajil <sajilck@gmail.com>
2023-10-27 12:53:41 +0530

    minor modification

--- example_clf/example_dnn.ino
+++ example_clf/example_dnn.ino
@@ -11,11 +11,11 @@ void setup() {
 
 void loop() {
     
-        float input[2] = {0.31699103, -2.38886223};
+        float input[2] = {0.3169, -2.3888};
         float y_pred = dnn_model.predict(input);
         
         Serial.println("Expected output: ");
-        Serial.println(-48.130339359863974);
+        Serial.println(-48.1303);
         
         Serial.println("Predicted output:");
         Serial.println(y_pred);
